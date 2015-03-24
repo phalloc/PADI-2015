1c26b6f74b9f1a39c5a06556bd6d5cd6e71ca77d
phalloc <tiagodiogo@netcabo.pt>
2015-03-24 12:03:55 +0000

    skeleton

---
+++ Project/MapNoReduce/Node/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Node
+{
+    public class Node
+    {
+
+        private long id;
+
+        public long getId()
+        {
+            return id;
+        }
+
+        public void setId(long id)
+        {
+            this.id = id;
+        }
+    }
+}
