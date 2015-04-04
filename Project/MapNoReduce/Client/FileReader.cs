087a765c5cdf00ee2bffa930b27fbac82942efaa
GitTiago <tiagomanuelfs@hotmail.com>
2015-04-04 19:41:09 +0100

    Crude FileReader Added - Use at own risk

--- Project/MapNoReduce/Client/FileReader.cs
+++ Project/MapNoReduce/Client/FileReader.cs
@@ -10,28 +10,83 @@ namespace Client
     {
         private string _path;
         private FileStream _reader;
-
+        UTF8Encoding encoding;
 
         public FileReader(string path)
         {
             _path = path;
             _reader = new FileStream(_path, FileMode.Open);
-
-            long fileSize = _reader.Length;
-
-
-
-
+            encoding = new UTF8Encoding(true);
         }
 
         public string fetchSplitFromFile(long begin, long end)
         {
-
+            long beginLine;
+            long endLine;
             //FIXME: Tratamento Excepção
             /*if(begin > _reader.length || begin > end || end > _reader.Length || begin < 0){
 
             }*/
 
+
+
+            if (begin != 0)
+            {
+
+                //verifica se o split dividiu a linha perfeitamente
+                _reader.Seek(begin - 1, SeekOrigin.Begin);
+                byte[] singleByte = new byte[1];
+
+                _reader.Read(singleByte, 0, 1);
+
+                if (encoding.GetString(singleByte) == "\n")
+                {
+                    beginLine = begin;
+                }
+                else
+                {
+                    //colocar found a false se -1
+                    beginLine = seekBeginLine(begin, end);
+
+                }
+
+            }
+            else
+            {
+                beginLine = begin;
+                _reader.Seek(begin, SeekOrigin.Begin);
+
+            }
+
+            endLine = seekNewLine(end);
+
+
+            string split = readString(beginLine, endLine);
+
+
+            return split;
+        }
+
+        private long seekNewLine(long end)
+        {
+
+            _reader.Seek(end - 1, SeekOrigin.Begin);
+            //FIXME: Should be optimized
+            byte[] singleByte = new byte[1];
+            while (_reader.Read(singleByte, 0, 1) > 0)
+            {
+                if (_reader.Position == getFileSize() || encoding.GetString(singleByte) == "\n")
+                {
+                    return _reader.Position;
+                }
+            }
+
+            //Fixme: switch to appropriate exception(even tho its unreachable)
+            throw new Exception();
+        }
+
+        private string readString(long begin, long end)
+        {
             Console.WriteLine("FileSize: " + _reader.Length);
 
             _reader.Seek(begin, SeekOrigin.Begin);
@@ -40,7 +95,7 @@ namespace Client
 
             int readBufferLength = 12;
             byte[] readBuffer = new byte[readBufferLength];
-            UTF8Encoding encoding = new UTF8Encoding(true);
+
 
 
             int nBytesToRead = getBytesToRead(end, readBufferLength);
@@ -60,10 +115,31 @@ namespace Client
 
 
             }
-
-
-
             return split;
+        }
+
+        private long seekBeginLine(long start, long end)
+        {
+
+            _reader.Seek(start, SeekOrigin.Begin);
+            //FIXME: Should be optimized
+            byte[] singleByte = new byte[1];
+            while (_reader.Read(singleByte, 0, 1) > 0)
+            {
+                if (_reader.Position >= end)
+                {
+                    //Fixme
+                    throw new Exception();
+                }
+
+                if (encoding.GetString(singleByte) == "\n")
+                {
+                    return _reader.Position;
+                }
+            }
+
+            //Fixme
+            throw new Exception();
         }
 
         private int getBytesToRead(long end, int readBufferLength)
@@ -111,7 +187,7 @@ namespace Client
 
             Console.WriteLine("Each split will have " + fs.getSplitSize(3) + " bytes");
 
-            string result = fs.fetchSplitFromFile(85, 102);
+            string result = fs.fetchSplitFromFile(55, 102);
             Console.WriteLine("Got: " + result);
             Console.ReadLine();
 
